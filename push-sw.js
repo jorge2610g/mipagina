5348c61b131e8c5e61a769a755a99c69c18d0185
jorge2610g <scuentas150@gmail.com>
2026-09-22 15:16:08 -0300

    Admite notificaciones silenciosas y alerta fuerte para QR

--- push-sw.js
+++ push-sw.js
@@ -1,6 +1,17 @@
 self.addEventListener("push",event=>{
  let data={};try{data=event.data?.json()||{}}catch{data={body:event.data?.text()||"Tienes una actualización."}}
- event.waitUntil(self.registration.showNotification(data.title||"YummyPro",{body:data.body||"Tienes una actualización.",icon:"/icon-192.png",badge:"/icon-192.png",tag:data.tag||"yummypro-notification",renotify:true,data:{url:data.url||"/"}}));
+ const options={
+  body:data.body||"Tienes una actualización.",
+  icon:"/icon-192.png",
+  badge:"/icon-192.png",
+  tag:data.tag||"yummypro-notification",
+  renotify:true,
+  silent:!!data.silent,
+  requireInteraction:!!data.requireInteraction,
+  data:{url:data.url||"/"}
+ };
+ if(!data.silent)options.vibrate=[220,100,220,100,350];
+ event.waitUntil(self.registration.showNotification(data.title||"YummyPro",options));
 });
 self.addEventListener("notificationclick",event=>{
  event.notification.close();
