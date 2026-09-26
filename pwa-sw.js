dd2b7e82ec962700a5e4913caabf4453f586b53e
jorge2610g <scuentas150@gmail.com>
2026-09-26 01:51:17 -0300

    chore(staging): refresh PWA cache for paginated public catalog

--- pwa-sw.js
+++ pwa-sw.js
@@ -1,4 +1,4 @@
-const YUMMYPRO_CLIENT_CACHE="yummypro-client-v1636-scalability";
+const YUMMYPRO_CLIENT_CACHE="yummypro-client-v1637-public-catalog";
 const CLIENT_CORE=["/offline.html","/manifest.webmanifest","/pwa-icon.svg","/icon-192.png","/icon-512.png","/apple-touch-icon.png","/white-label-icon.svg"];
 self.addEventListener("install",event=>{
  event.waitUntil(caches.open(YUMMYPRO_CLIENT_CACHE).then(c=>c.addAll(CLIENT_CORE)));
